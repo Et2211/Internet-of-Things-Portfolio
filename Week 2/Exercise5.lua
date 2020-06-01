e2aa79415260502d5d4486b8a1a02fe6edb86212
et2211 <up817427@myport.ac.uk>
2020-06-01 19:20:39 +0100

    edit ex 5

--- Week 2/Exercise5.lua
+++ Week 2/Exercise5.lua
@@ -1,28 +1,44 @@
-dc=1023
+dc = 1023 
+positive=true 
 pinDim = 3
-pwm.setup(pinDim,1000,dc)
+inputs = {500, 300}
+index = 0
+pwm.setup(pinDim, 500, dc) -- 1000 is the rate of change.
 pwm.start(pinDim)
-pause=false
-mytimer = tmr.create()
-mytimer:alarm(200,tmr.ALARM_AUTO,function()
+blinkDim = tmr.create()
+blinkDim:alarm(200, tmr.ALARM_AUTO, function()
+    flashDim(inputs)
+end)
 
-    if pause==false then
-        dc=dc-10
-        print(dc)
-        pwm.setduty(pinDim,dc)
-        if (dc<10) then
-            dc=1023
-        end
+function dimLights()
+    if (positive == true) then
+        dc=dc-10 
+    else
+        dc=dc+10 
     end
-end)
-
-mytimer2 = tmr.create()
-mytimer2:alarm(5000, tmr.ALARM_AUTO, function()
-    pause = true
-    pwm.setduty(pinDim, 0)
+        print(dc)
+        pwm.setduty(pinDim,dc)  
     
-    mytimer2:alarm(3000, tmr.ALARM_SINGLE, function()
-        pwm.setduty(pinDim, dc)
-        pause = false
-    end)    
-end)
+    if(dc<10) then 
+        positive = false
+    end
+    
+    if(dc>1013) then 
+        positive = true
+    end
+end
+
+function flashDim(inputs)
+    index = index + 1
+    if index == #inputs + 1  then
+        index = 1
+    end 
+    
+    print(index)
+    blinkDim:interval(inputs[index] + 1)
+    if index == 1 then
+        dimLights()
+    else
+        pwm.setduty(pinDim, 0)
+    end
+end
