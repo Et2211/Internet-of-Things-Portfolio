9d7874c5a7b0ab9fce4507841167e4a1457f88f6
et2211 <up817427@myport.ac.uk>
2020-06-02 02:40:24 +0100

    updated Cron functions

--- Week 7-8/Exercise1.lua
+++ Week 7-8/Exercise1.lua
@@ -1,6 +1,28 @@
-tmsrv = "uk.pool.ntp.org"
---an optional NTP server
-mytimer=tmr.create()
+wifi.sta.sethostname("uopNodeMCU")
+wifi.setmode(wifi.STATION)
+station_cfg={}
+station_cfg.ssid="BT-3KA62F"
+station_cfg.pwd="MvfRVvE6gEnvpL"
+station_cfg.save=true
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
+
+mytimer = tmr.create()
+mytimer:register(3000, 1, function() 
+   if wifi.sta.getip()==nil then
+        print("Connecting to AP...\n")
+   else
+        sntp.sync(tmsrv,function()
+        print("Sync succeeded")
+        mytimer:stop()
+        stampTime()
+    end,function()
+        print("Synchronization failed!")
+    end, 1)
+   
+   end 
+end)
+mytimer:start()
 
 function stampTime()
     --get the stamp of the time from synchronized clock
@@ -9,13 +31,3 @@ function stampTime()
     print(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"],
     tm["hour"], tm["min"], tm["sec"]))
 end
-
-mytimer:alarm(2000, 1, function()
-    sntp.sync(tmsrv,function()
-        print("Sync succeeded")
-        mytimer:stop()
-        stampTime()
-    end,function()
-        print("Synchronization failed!")
-    end, 1)
-end)
--- Week 7-8/Exercise2.lua
+++ Week 7-8/Exercise2.lua
@@ -1,29 +1,28 @@
---cron.schedule()
---refer to https://en.wikipedia.org/wiki/Cron
---for the mask "* * * * *" information
---minute, hour, day of a month, month of a year, day of the week
+wifi.sta.sethostname("uopNodeMCU")
+wifi.setmode(wifi.STATION)
+station_cfg={}
+station_cfg.ssid="BT-3KA62F"
+station_cfg.pwd="MvfRVvE6gEnvpL"
+station_cfg.save=true
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
 
-tmsrv = "uk.pool.ntp.org"
---an optional NTP server
-mytimer=tmr.create()
-
-function stampTime()
-    --get the stamp of the time from synchronized clock
-    sec,microsec,rate = rtctime.get()
-    tm = rtctime.epoch2cal(sec,microsec,rate)
-    print(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"],
-    tm["hour"], tm["min"], tm["sec"]))
-end
-
-mytimer:alarm(2000, 1, function()
-    sntp.sync(tmsrv,function()
+mytimer = tmr.create()
+mytimer:register(5000, 1, function() 
+   if wifi.sta.getip()==nil then
+        print("Connecting to AP...\n")
+   else
+        sntp.sync(tmsrv,function()
         print("Sync succeeded")
         mytimer:stop()
         stampTime()
     end,function()
         print("Synchronization failed!")
     end, 1)
+   
+   end 
 end)
+mytimer:start()
 
 cron.schedule("* * * * *", function(e)
 print("For every minute function will be executed once")
@@ -35,6 +34,10 @@ cron.schedule("0 7 * * *", function(e)
 print("\n Alarm Clock \n It is 07:00!!! \n Get UP! \n")
 end)
 
---set your own alarm at 07:00
---please change it to 2 minutes later from now on
---and see if it will alarm you according to the synchronized time.
+function stampTime()
+    --get the stamp of the time from synchronized clock
+    sec,microsec,rate = rtctime.get()
+    tm = rtctime.epoch2cal(sec,microsec,rate)
+    print(string.format("%04d/%02d/%02d %02d:%02d:%02d", tm["year"], tm["mon"], tm["day"],
+    tm["hour"], tm["min"], tm["sec"]))
+end
